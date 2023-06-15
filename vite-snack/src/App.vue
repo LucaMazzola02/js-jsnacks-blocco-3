1a2bf50e7cd04dd46771f756bde0dfff01e786d8
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-06-15 15:23:36 +0200

    create vite-snack

---
+++ vite-snack/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppHeader from './components/HeaderApp.vue';
+
+  export default {
+    name: 'App',
+    components:{
+      AppHeader
+    }
+}
+
+</script>
+
+<template>
+  <AppHeader />
+  <GeneralVariable/>
+</template>
+
+<style lang="scss">
+ @import './styles/general.scss';
+</style>
---
+++ vite-snack/src/components/HeaderApp.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+    name: 'AppHeader',
+    data(){
+        return{
+            links : [
+                'Home',
+                'Contacts',
+                'Listin',
+                'Instagram',
+            ]
+        }
+    }
+    
+};
+</script>
+
+<template>
+    <div class="nav-bar">
+        <ul>
+            <li v-for="link in links">
+                {{ link }}
+            </li>
+        </ul>
+    </div>
+  
+</template>
+
+<style lang="scss">
+
+
+</style>
